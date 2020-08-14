d3d79040bca976e8496aad5999dee02faf2e725a
Cyrus Tse <kaho0533@yahoo.com.hk>
2020-08-15 02:25:30 +0800

    add else part

--- driving.py
+++ driving.py
@@ -11,4 +11,5 @@ elif country == "America":
 		print('you can go to examine for the Driver License')
 	else:
 		print('you still can not to examine the Driver License')
-		
+else:
+	print('you simply enter the TaiWan and American country')
